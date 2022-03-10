9b5a45d7b1e795eed5cb5b50bd8190277ff3b0a5
Chad Powell <chad.powell@student.awesomeinc.org>
2022-03-10 02:04:23 +0000

    update

--- js/appData.js
+++ js/appData.js
@@ -1,30 +1,36 @@
-const WIN_COND =[
-    [0, 1, 2],   
-    [3, 4, 5],    
-    [6, 7, 8],   
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8]
-    [0, 4, 8],
-    [2, 4, 6]
+const WIN_COND = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8][(0, 4, 8)],
+	[2, 4, 6],
 ];
 
 const HTML = [
 	{
-		type: "div",
-		class: "h1",
-        text: "Tic Tac Toe"
+		div: 'div',
+		class: 'h1 text-center mb-5',
+		text: 'Tic Tac Toe',
 	},
 
 	{
-		type: "div",
-		class: "row md-4",
+		div: 'div',
+		class: 'row d-flex justify-content-center px-5 mx-5',
 	},
 
 	{
-		type: "div",
-		class: "col-4 borders fs-1",
+		div: 'div',
+		class: 'col-4 pt-5 pb-5 text-center border border-success fs-1',
+		type: "button"
+
+	},
+
+	{
+		div: "div",
+		type: 'button',
+		class: 'col-4 btn btn-danger btn-lg mt-5',
+		text: 'Reset Game'
 	}
-]
-
-
+];
--- js/main.js
+++ js/main.js
@@ -1,19 +1,95 @@
 // Model
 class Game {
-	constructor() {}
+	constructor() {
+		this.board = [];
+		this.currentPlayer = 'X';
+		this.turn = 0;
+		this.clicked = false;
+		this.gameOver = false;
+
+	}
+
+	setState(i, e) {
+
+		if(!this.gameOver && this.board[i].clicked) {
+			if (this.turn % 2 == 1) {
+
+				this.board[i].textContent = 'X';
+			}
+
+			else {
+
+				this.board[i].textContent = 'O';
+			}
+
+			this.board[i].clicked = true;
+			this.turn++;
+
+			
+		}
+	}
 }
 
 // View
 class View {
-	constructor() {}
+	constructor() {
+		this.restartButton = null;
+		
+	}
+
+	render() {
+
+		// GENERATE TITLE
+		let app = document.getElementById('app');
+		let title = document.createElement(`${HTML[0].div}`);
+		title.setAttribute('class',`${HTML[0].class}`);
+		title.style.marginTop = '5%';
+		title.style.fontWeight = 'bold';
+		title.style.color = 'black';
+		title.style.text = `${HTML[0].align}`
+		title.textContent = `${HTML[0].text}`
+		app.appendChild(title);
+
+		//GENERATE BOARD
+		let row = document.createElement(`${HTML[1].div}`);
+		row.setAttribute('class', `${HTML[1].class}`);
+		for(let i = 0; i < 9; i++) {
+			let col = document.createElement(`${HTML[2].div}`);
+			col.setAttribute('class', `${HTML[2].class}`);
+			col.setAttribute('type', `${HTML[2].type}`);
+			col.addEventListener('click', (e) => {
+				e.target.enabled = true;
+				console.log(e)
+			})
+			row.appendChild(col);
+			
+		}
+		app.appendChild(row);
+		
+		//RESET BUTTON
+		let resetButton = document.createElement(`${HTML[3].div}`);
+		resetButton.setAttribute('type', `${HTML[3].type}`);
+		resetButton.setAttribute('class', `${HTML[3].class}`);
+		resetButton.textContent = `${HTML[3].text}`;
+		row.appendChild(resetButton);
+
+		this.restartButton.addEventListener('click', )
+	}
 }
 
 // Controller
 class Controller {
-	constructor() {}
+	constructor() {
+		this.m = new Game();
+		this.v = new View();
+		
+	}
+
+	run() {
+		this.v.render();
+	}
 }
 
-// Tile
-class Tile {
-	constructor() {}
-}
+// new Controller
+let game = new Controller();
+game.run();
