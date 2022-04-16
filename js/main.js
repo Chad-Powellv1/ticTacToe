1c43158e3c3b7309f57832c99f5b3808856e0206
Chad Powell <chad.powell@student.awesomeinc.org>
2022-04-16 17:47:10 +0000

    save

--- js/main.js
+++ js/main.js
@@ -1,102 +1,85 @@
-// Model  REMEMBER ORDER IS IMPORTANT!!!
+class Event {
+	constructor() {
+		this.listeners = [];
+	}
+
+	addListener(listener) {
+		this.listeners.push(listener);
+	}
+
+	trigger(params) {
+		this.listeners.forEach(listener => { listener(params); });
+	}
+};
+
 class Game {
 	constructor() {
-		this.board = [];
+		this.board = Array(9).fill();
 		this.currentPlayer = 'X';
-		this.turn = 0;
 		this.gameOver = false;
-		
-
-
-		
+		this.updateTileEvent = new Event();
+		this.setWinEvent = new Event();
+		this.setDrawEvent = new Event();
 	}
 
-	gameState = () => {
-		this.winCondition = [
-			[0, 1, 2],
-			[3, 4, 5],
-			[6, 7, 8],
-			[0, 3, 6],
-			[1, 4, 7],
-			[2, 5, 8],
-			[0, 4, 8],
-			[2, 4, 6],
-		];
-		
-		for (let i = 0; i < this.winCondition.length; i++) {
-			let sum = 0;
-			for (let j = 0; j < this.winCondition[i].length; j++) {
-				sum += this.board[this.winCondition[i][j]].dataset.index;
-				if(sum === 3) {
-					
-					this.gameOver = true;
-					this.note.textContent = "X is the winner! 👏";
-					this.turn.textContent = '';
-				}
-
-				if(sum === 15) {
-
-					this.gameOver = true;
-					this.note.textContent = 'O is the winner! 👏';
-					this.turn.textContent = '';
-				}
-			}
-			sum = 0;
+	setTurn(move) {
+		if (this.gameOver || move < 0 || move > 8 || this.board[move]) {
+			return false;
 		}
 
-		if(this.turn === 9 && this.gameOver) {
-			this.gameOver = true;
-			this.note.textContent = 'No winner, the game is a draw. 😵'
-			this.turn.textContent = '';
+		this.board[move] = this.currentPlayer;
+		this.updateTileEvent.trigger({
+			move, player: this.currentPlayer
+		});
+
+		this.gameOver = this.win() || this.draw();
+
+		if (!this.gameOver) {
+			this.setPlayer();
 		}
+
+		return true;
 	}
 
-	setState = (i, e, gameState) => {
-		
-		if(!this.gameOver) { 
-			
-			if (this.turn % 2 == 0) {
+	setWin() {
+		const wins = WIN_COND.some(win => this.boar[1[0]]
+			&& this.board[1[0]] === this.board[1[1]]
+			&& this.board[1[1]] === this.board[1[2]])
 
-				this.board[i].textContent = this.currentPlayer;
-				this.board[i].dataset.index = 1;
-				// display player X turn
-			}
-
-			else {
-
-				this.board[i].textContent = 'O';
-				this.board[i].dataset.index= 5;
-				// display player O turn
-			}
-
-			this.turn++;
-
-			if(this.turn >= 5) {
-				this.gameState();
-			}
-
-			
+		if (wins) {
+			this.setWinEvent.trigger(this.currentPlayer);
 		}
 
+		return wins;
 	}
 
+	setDraw() {
+		const draw = this.board.every(i => i);
 
-}
+		if (draw) {
+			this.setDrawEvent.trigger();
+		}
+
+		return draw;
+	}
+
+	setPlayer() {
+		this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+	}
+};
 
 // View
 class View {
-	constructor(model) {
-		this.m = model;
-		this.restartButton = null;
-		
+	constructor() {
+		this.setTurnEvent = new Event();
 	}
 
-	render(setState,board) {
+	render() {
 
 		// GENERATE TITLE
-		let app = document.getElementById('app');
-		let title = document.createElement(`${HTML[0].div}`);
-		title.setAttribute('class',`${HTML[0].class}`);
+		const app = document.getElementById('app');
+		const title = document.createElement(`${HTML[0].div}`);
+		title.className = `${HTML[0].class}`;
 		title.style.marginTop = '5%';
 		title.style.fontWeight = 'bold';
 		title.style.color = 'black';
@@ -105,30 +88,34 @@ class View {
 		app.appendChild(title);
 
 		//GENERATE BOARD
-		let row = document.createElement(`${HTML[1].div}`);
-		row.setAttribute('class', `${HTML[1].class}`);
-		for(let i = 0; i < 9; i++) {
-			let col = document.createElement(`${HTML[2].div}`);
-			col.setAttribute('class', `${HTML[2].class}`);
-			col.setAttribute('type', `${HTML[2].type}`);
-			col.setAttribute('data-index', i);
-			col.addEventListener('click', (e) => {
-				setState(i,e)
-				console.log(e)
-			}, { once: true })
-			board.push(col)
-			
-			
-			row.appendChild(col);
-		}
-		app.appendChild(row);
-		
+		const gameBoard = document.createElement(`${HTML[1].div}`);
+		gameBoard.className = `${HTML[1].class}`;
+
+		this.tiles = Array(9).fill().map((_, i) => {
+
+			const tile = document.createElement(`${HTML[2].div}`);
+			tile.className = `${HTML[2].class}`;
+			tile.type = `${HTML[2].type}`;
+			tile.setAttribute('data-index', i);
+
+			tile.addEventListener('click', () => {
+				this.setTurnEvent.trigger(i);
+			});
+
+			gameBoard.appendChild(tile);
+
+			return tile;
+
+		});
+
+		app.appendChild(gameBoard);
+
 		//RESET BUTTON
 		let resetButton = document.createElement(`${HTML[3].div}`);
 		resetButton.setAttribute('type', `${HTML[3].type}`);
 		resetButton.setAttribute('class', `${HTML[3].class}`);
 		resetButton.textContent = `${HTML[3].text}`;
-		row.appendChild(resetButton);
+		gameBoard.appendChild(resetButton);
 
 		// this.restartButton.addEventListener('click', )
 
@@ -137,21 +124,38 @@ class View {
 		note.setAttribute('class', `${HTML[4].class}`)
 		resetButton.appendChild(note);
 	}
-}
+
+	updateTile(i) {
+		this.tiles[i.move].innerHTML = i.currentPlayer;
+	}
+
+	// winner(winner) {
+	// 	this.note.innerHTML = `${winner} wins!`;
+	// }
+
+	// draw() {
+	// 	this.note.innerHTML = `The game is a draw!`;
+	// }
+};
 
 // Controller
 class Controller {
 	constructor() {
 		this.m = new Game();
 		this.v = new View();
-		this.v.render(this.m.setState,this.m.board,this.m.gameState)
+		this.v.setTurnEvent.addListener(move => { this.m.setTurn(move); });
+		this.m.updateTileEvent.addListener(i => { this.v.updateTile(i); });
+		this.m.setWinEvent.addListener(winner => { this.v.winner(winner); });
+		this.m.setDrawEvent.addListener(() => { this.v.draw(); });
 	}
 
 	run() {
 		this.v.render();
 	}
-}
+
+
+};
+
 
 // new Controller
 let game = new Controller();
-
